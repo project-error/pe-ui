ce263670e97be6ff44df92e20f0e3172954bf30d
Taso <taso@projecterror.dev>
2021-08-09 12:24:47 -0400

    feat(ui/toast): Added toast provider:

--- web/src/components/MainWrapper.tsx
+++ web/src/components/MainWrapper.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
 import { TextPrompt } from './TextPrompt';
+import { ToastProvider } from '../providers/ToastProvider';
+// import { SettingsModal } from './SettingsModal';
 
 const MainWrapper: React.FC = () => {
   return (
-    <Box h='100%' w='100%' p={4} bg='none'>
-      <TextPrompt />
-    </Box>
+    <ToastProvider>
+      <Box h='100%' w='100%' p={4} bg='none'>
+        <TextPrompt />
+        {/*<SettingsModal />*/}
+      </Box>
+    </ToastProvider>
   );
 };
 
---
+++ web/src/providers/ToastProvider.tsx
@@ -0,0 +1,64 @@
+import React, { createContext, useCallback } from 'react';
+import { ToastId, ToastOptions, useToast } from '@chakra-ui/react';
+import { useNuiEvent } from '../hooks/useNuiEvent';
+import { debugData } from '../utils/debugData';
+
+interface ToastOpts {
+  position: ToastOptions['position'];
+  status?: 'success' | 'error' | 'warning' | 'info';
+  id: ToastOptions['id'];
+  description: string;
+}
+
+interface ToastCtxValue {
+  addPersistentToast: (toastOpts: ToastOpts) => void;
+  clearPersistentToast: (id: ToastOptions['id']) => void;
+}
+
+const ToastCtx = createContext<null | ToastCtxValue>(null);
+
+debugData<ToastOpts>([
+  {
+    action: 'addPersistentToast',
+    data: {
+      id: 'niceToast',
+      position: 'bottom-left',
+      status: 'info',
+      description: 'You fucking suck',
+    },
+  },
+]);
+
+export const ToastProvider: React.FC = ({ children }) => {
+  const toast = useToast();
+
+  const addPersistentToast = useCallback(
+    (toastOpts: ToastOpts) => {
+      toast({
+        id: toastOpts.id,
+        description: toastOpts.description,
+        position: toastOpts.position,
+        status: toastOpts.status,
+        isClosable: false,
+        duration: null,
+      });
+    },
+    [toast]
+  );
+
+  const clearPersistentToast = useCallback(
+    (toastId: ToastId) => {
+      toast.close(toastId);
+    },
+    [toast]
+  );
+
+  useNuiEvent<ToastOpts>('addPersistentToast', addPersistentToast);
+  useNuiEvent<ToastId>('clearPersistentToast', clearPersistentToast);
+
+  return (
+    <ToastCtx.Provider value={{ addPersistentToast, clearPersistentToast }}>
+      {children}
+    </ToastCtx.Provider>
+  );
+};
