a50ad68e6e8653e507c9cf231005ca5160017647
Taso <taso@projecterror.dev>
2021-08-09 21:12:33 -0400

    fix(scripts/prompts): Misc fixes and tweaks

--- client/prompt.lua
+++ client/prompt.lua
@@ -1,5 +1,5 @@
 -- Will track whether prompt is currently open
-local promptIsOpen = fallse
+local promptIsOpen = false
 
 --[[
 table object:
@@ -14,7 +14,7 @@ table object:
 ---@param cb function -- Will respond with either
 ---
 --- NOTE: Need to handle an export spam triggering many prompts
-function startPrompt(promptTable)
+local function startPrompt(promptTable)
   debugPrint('Prompt opened >')
   debugPrint(json.encode(promptTable))
 
@@ -25,19 +25,20 @@ function startPrompt(promptTable)
 
   promptIsOpen = true
 
-  -- Data will be an array with ['closed' | 'submitted', content | null]
-  RegisterNUICallback(cbStr, function(data, cb)
+  RegisterNuiCallbackType(cbStr)
+  local eventData = AddEventHandler('__cfx_nui:' .. cbStr, function(data, cb)
     p:resolve(data)
     promptIsOpen = false
-    -- TODO: This causes a memory leak at the current moment
-    -- as the RegisteredType is never cleaned up from the ResourceUI
-    -- container in cpp. Feature pending
-    RemoveEventHandler('__cfx_nui:' .. cbStr)
     cb({})
   end)
 
   local result = Citizen.Await(p)
+  -- TODO: This causes a memory leak at the current moment
+  -- as the RegisteredType is never cleaned up from the ResourceUI
+  -- container in cpp. Feature pending
+  RemovEventHandler(eventData)
 
+  -- Returns two results as ['closed' | 'submitted', content | null]
   return result[1], result[2]
 end
 
