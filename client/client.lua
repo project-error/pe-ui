b8f8a1751b06c6ac08fabda8dfc3982062c244bf
Taso <taso@projecterror.dev>
2021-08-09 21:16:55 -0400

    chore(misc): General misc cleanup of unneeded functions

--- client/client.lua
+++
@@ -1,9 +0,0 @@
-local function toggleNuiFrame(shouldShow)
-  SetNuiFocus(shouldShow, shouldShow)
-  SendReactMessage('setVisible', shouldShow)
-end
-
-RegisterCommand('show-nui', function()
-  toggleNuiFrame(true)
-  debugPrint('Show NUI frame')
-end)
